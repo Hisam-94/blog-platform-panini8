bee5214e0508cf73a6441b123ecc8e17c6ba9347
Hisamuddin Shaikh <hisamuddin94@gmail.com>
2025-04-29 01:09:54 +0530

    Added frontend url in cors

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,12 +2,12 @@ import dotenv from "dotenv";
 import path from "path";
 
 // Load environment variables from .env file
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config();
 
 const config = {
   port: process.env.PORT || 5000,
   mongodbUri:
-    process.env.MONGODB_URI || "mongodb://localhost:27017/blog_platform",
+    process.env.MONGODB_URI || "",
   jwtSecret: process.env.JWT_SECRET || "default_jwt_secret",
   jwtExpire: process.env.JWT_EXPIRE || "7d",
 };
